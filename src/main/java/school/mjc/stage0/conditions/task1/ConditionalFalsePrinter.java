7da40cbda6ac1c10e6140740251e045e9900d40e
Sergey234 <mostov.sergey88@gmail.com>
2022-11-01 15:26:38 +0300

    stage0-module4-conditions-task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false) {
+            System.out.println(false);
+        } else {
+            System.out.println(true);
+        }
     }
 }
